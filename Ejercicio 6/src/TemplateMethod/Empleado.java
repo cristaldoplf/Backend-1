6b836381808fdf8f45466408c5346a04de2642a1
cristaldoplf <cristaldoplf@gmail.com>
2022-03-04 17:54:14 -0300

    ejercicio 6 terminado, falta el test

--- Ejercicio 6/src/TemplateMethod/Empleado.java
+++ Ejercicio 6/src/TemplateMethod/Empleado.java
@@ -1,5 +1,64 @@
 package TemplateMethod;
 
 public abstract class Empleado {
-    private nombre
+    private String nombre;
+    private String apellido;
+    private int numeroCuenta;
+
+    public Empleado(String nombre, String apellido, int numeroCuenta) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public void liquidarSueldo() {
+        double sueldoTotal = calcularSueldo();
+        if (sueldoTotal < 0.1) {
+            try {
+                throw new Exception("Error, al intentar calcular la liquidacion, el sueldo no puede ser 0 o menor.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            String recibo = generarRecibo(sueldoTotal);
+            depositarImporte(sueldoTotal, recibo);
+        }
+
+    }
+
+    public abstract double calcularSueldo();
+
+    public abstract String generarRecibo(double monto);
+
+    public void depositarImporte(double monto, String recibo) {
+        if (monto > 0) {
+            System.out.println("La liquidacion generada es un documento " + recibo + ". Saldo a liquidar: " + monto + ".");
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
 }
+
+//“La liquidación generada es un documento impreso. Saldo a liquidar: 840”.
