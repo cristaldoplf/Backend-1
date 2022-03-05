aef086e916266bf4da9841c23fb377f80edd82fb
cristaldoplf <cristaldoplf@gmail.com>
2022-03-05 16:20:29 -0300

    Trabajo 4 terminado, falta el test

--- Ejercicio 4/src/Flyweight/ListaDeReproduccion.java
+++ Ejercicio 4/src/Flyweight/ListaDeReproduccion.java
@@ -1,4 +1,34 @@
 package Flyweight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaDeReproduccion {
+    private String nombre;
+    private List<Cancion> lista;
+
+    public ListaDeReproduccion(String nombre) {
+        this.nombre = nombre;
+        lista = new ArrayList<>();
+    }
+
+    public void addCancion(CancionFactory factory, String nombre, String artista, String genero){
+        lista.add(factory.getCancion(nombre,artista,genero));
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Cancion> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Cancion> lista) {
+        this.lista = lista;
+    }
 }
